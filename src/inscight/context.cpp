86cdfc38f4a95097fd19e016b73ac597be1a9e10
pau-ribelles <pau@mwa.re>
2025-03-05 15:34:25 +0100

    fix: copyright box format

--- src/inscight/context.cpp
+++ src/inscight/context.cpp
@@ -1,15 +1,19 @@
-/*
- * Copyright (C) 2022-2025 MachineWare GmbH  
- *
- * This file is licensed for non-commercial use only.
- * You may use, modify, and distribute this file for personal or educational purposes,
- * but any commercial use, including but not limited to selling, licensing, or
- * integrating this code into proprietary software, is strictly prohibited unless otherwise agreed to in writing by MachineWare GmbH.
- *
- * THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES,
- * OR OTHER LIABILITY ARISING FROM THE USE OF THIS SOFTWARE.
- */
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2022-2025 MachineWare GmbH                                   *
+ *                                                                            *
+ * This file is licensed for non-commercial use only.                         *
+ * You may use, modify, and distribute this file for personal or educational  *
+ * purposes, but any commercial use, including but not limited to selling,    *
+ * licensing, or integrating this code into proprietary software, is strictly *
+ * prohibited unless otherwise agreed to in writing by MachineWare GmbH.      *
+ *                                                                            *
+ * THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS   *
+ * OR IMPLIED. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE   *
+ * FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY ARISING FROM THE USE OF         *
+ * THIS SOFTWARE.                                                             *
+ *                                                                            *
+ ******************************************************************************/
 
 #include "inscight/context.h"
 #include "inscight/database.h"

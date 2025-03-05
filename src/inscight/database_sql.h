1007825388892784531b9479fff3b1515481ce19
pau-ribelles <pau@mwa.re>
2025-03-05 15:00:22 +0100

    fix: Inscight copyright headers + INSCI^CT_LICENS

--- src/inscight/database_sql.h
+++ src/inscight/database_sql.h
@@ -1,13 +1,15 @@
-/******************************************************************************
- *                                                                            *
- * Copyright 2023 MachineWare GmbH                                            *
- * All Rights Reserved                                                        *
- *                                                                            *
- * This is unpublished proprietary work owned by MachineWare GmbH. It may be  *
- * used, modified and distributed in accordance to the license specified by   *
- * the license file in the root directory of this project.                    *
- *                                                                            *
- ******************************************************************************/
+/*
+ * Copyright (C) 2022-2025 MachineWare GmbH  
+ *
+ * This file is licensed for non-commercial use only.
+ * You may use, modify, and distribute this file for personal or educational purposes,
+ * but any commercial use, including but not limited to selling, licensing, or
+ * integrating this code into proprietary software, is strictly prohibited unless otherwise agreed to in writing by MachineWare GmbH.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES,
+ * OR OTHER LIABILITY ARISING FROM THE USE OF THIS SOFTWARE.
+ */
 
 #ifndef INSCIGHT_DATABASE_SQL_H
 #define INSCIGHT_DATABASE_SQL_H
